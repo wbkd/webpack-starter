f850d8e52cef6ee569dcd12368ab31ac9b42bd4a
Moritz <info@moritzklack.com>
2018-10-02 11:23:47 +0200

    feat(webpack): handle mjs files

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -33,6 +33,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      },
+      {
         test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
         use: {
           loader: 'file-loader',

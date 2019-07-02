38477c4f976b448e771f0d939c32452750119b50
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-02 17:18:49 -0600

    Adds get /:id to foods router using Objective

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -16,9 +16,13 @@ router.get('/', async (req,res) => {
   res.send(foods)
 })
 
+router.get('/:id', async (req,res) => {
+  // const food = await Food.query().findById(req.params.id).joinRelation('meals')
+  const food = await Food.query().findById(req.params.id)
+  res.send(food)
+})
 
 router.post('/', (req,res) => {
-  eval(pry.it)
   database('foods').insert({name: req.query.name, calories: req.query.calories})
     .then()
 })

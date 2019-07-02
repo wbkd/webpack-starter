91836d7bf2603c6be9351ecaea42b2d754a19b97
Ethan Grab <ethangrab@gmail.com>
2019-07-02 15:21:05 -0600

    meal model creation

---
+++ models/meal.js
@@ -0,0 +1,31 @@
+const knex = require('knex')
+const connection require('../knexfile')
+const { Model } = require('objection')
+
+const knexConnection = knex(connection)
+
+Model.knex(knexConnection)
+
+class Meal extends Model {
+  static get tableName () {
+    return 'meals'
+  }
+  static get relationMappings () {
+    return {
+      foods: {
+        relation: Model.ManyToManyRelation,
+        modelClass: Food,
+        join: {
+          from: 'meals.id',
+          through: {
+            from: 'food_meals.food_id',
+            to: 'food_meals.meal_id'
+          },
+          to: 'food.id'
+        }
+      }
+    }
+  }
+}
+
+module.exports = Food

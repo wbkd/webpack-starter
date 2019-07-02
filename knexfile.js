7f57aacc18194de5479fb0eb309e9a1b6872c0db
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-02 14:36:00 -0600

    Adds config data to knexfile
    Co-authored-by: Ethan Grab <40487417+Stoovles@users.noreply.github.com>

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,13 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/quant_self',
+    connection: {
+      host : '127.0.0.1',
+      user : 'walsh',
+      password : null,
+      database : 'quant_self',
+      charset: 'utf8'
+    },
     migrations: {
       directory: './db/migrations'
     },
@@ -14,9 +20,28 @@ module.exports = {
     useNullAsDefault: true
   },
 
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'quant_self_staging',
+      user:     'walsh',
+      password: null
+    },
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
+  },
+
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      database: 'quant_self_production',
+      user:     'walsh',
+      password: null
+    },
     migrations: {
       directory: './db/migrations'
     },
@@ -24,5 +49,4 @@ module.exports = {
       directory: './db/seeds/dev'
     }
   }
-
 };

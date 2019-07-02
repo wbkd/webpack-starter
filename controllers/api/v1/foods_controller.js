ac881402c3c2d01bea4d0fad1674ec9bb02d727b
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-02 17:17:31 -0600

    Adds get / to foods router using Objective

---
+++ controllers/api/v1/foods_controller.js
@@ -0,0 +1,13 @@
+// const express = require("express");
+// const fetch = require('node-fetch');
+// const router = express.Router();
+// const environment = process.env.NODE_ENV || 'development';
+// const configuration = require('../../../knexfile')[environment];
+// const database = require('knex')(configuration);
+// const food = require('../../../models/food')
+// var pry = require('pryjs');
+//
+// const index = (req,res) => {
+//   eval(pry.it);
+//   database('foods').select();
+// }
--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var Food = require('../../../models/food')
+// var Meal = require('../../../models/meal')
 var foodsController = require('../../../controllers/api/v1/foods_controller.js');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../../../knexfile')[environment];
+const database = require('knex')(configuration);
+var pry = require('pryjs');
 
-router.get('/', (req,res) => {
-  foodsController.index(req,res)
+const { Model } = require('objection');
+Model.knex(database)
+
+router.get('/', async (req,res) => {
+  const foods = await Food.query()
+  res.send(foods)
 })
+
+
+
 
 
 module.exports = router;

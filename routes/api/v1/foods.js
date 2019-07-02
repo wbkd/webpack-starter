18fb050cc68a4eb80838e68eca9c2f3330bc2f95
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-02 15:21:19 -0600

    Adds get route connecting the foodsController index

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -1,4 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var foodsController = require('../../../controllers/api/v1/foods_controller.js');
+
+router.get('/', (req,res) => {
+  foodsController.index(req,res)
+})
+
 
 module.exports = router;

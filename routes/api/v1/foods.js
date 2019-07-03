8ec6dd5c0bf6b678d17bbf63bbf4e8ccc1a06185
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-02 18:20:40 -0600

    Adds try-catch statements to all foods routes

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -12,61 +12,81 @@ const { Model } = require('objection');
 Model.knex(database)
 
 router.get('/', async (req,res) => {
-  const foods = await Food.query()
-  if (foods) {
-    res.send(foods)
-  }
-  else {
-    res.status(404).json({
-      error: "No foods here yet!"
-    })
-  }
+  try {
+    const foods = await Food.query()
+    if (foods) {
+      res.send(foods)
+    }
+    else {
+      res.status(404).json({
+        error: "No foods here yet!"
+      })
+    }
+  } catch (error) {
+      res.status(404).json({ error });
+    };
 })
 
 router.get('/:id', async (req,res) => {
   // const food = await Food.query().findById(req.params.id).joinRelation(Meal)
-  const food = await Food.query().findById(req.params.id)
-  if (food) {
-    res.send(food)
-  }
-  else {
-    res.status(404).json({
-      error: "No food exists with that ID"
-    })
-  }
+  try {
+    const food = await Food.query().findById(req.params.id)
+    if (food) {
+      res.send(food)
+    }
+    else {
+      res.status(404).json({
+        error: "No food exists with that ID"
+      })
+    }
+  } catch (error) {
+      res.status(404).json({ error });
+    };
 })
 
 router.post('/', async (req,res) => {
-  const newFood = await Food.query()
+  try {
+    const newFood = await Food.query()
     .insert({name: req.query.name, calories: req.query.calories})
-  res.send(newFood)
+    res.send(newFood)
+  } catch (error) {
+      res.status(404).json({ error });
+    };
 })
 
 router.patch('/:id', async (req,res) => {
-  const editedFood = await Food.query()
+  try {
+    const editedFood = await Food.query()
     .patchAndFetchById(req.params.id, req.query)
-  if (editedFood) {
-    res.send(editedFood)
-  }
-  else {
-    res.status(404).json({
-      error: "No food exists with that ID"
-    })
-  }
+    if (editedFood) {
+      res.send(editedFood)
+    }
+    else {
+      res.status(404).json({
+        error: "No food exists with that ID"
+      })
+    }
+  } catch (error) {
+      res.status(404).json({ error });
+    };
 })
 
 router.delete('/:id', async (req,res) => {
-  const deletedFood = await Food.query()
+  try {
+    const deletedFood = await Food.query()
     .findById(req.params.id)
-  if (deletedFood) {
-    await Food.query().findById(req.params.id).delete()
-    res.send(`${deletedFood.name} has been deleted.`)
-  }
-  else {
-    res.status(404).json({
-      error: "No food exists with that ID"
-    })
-  }
+    if (deletedFood) {
+      await Food.query().findById(req.params.id).delete()
+      res.send(`${deletedFood.name} has been deleted.`)
+    }
+    else {
+      res.status(404).json({
+        error: "No food exists with that ID"
+      })
+    }
+  } catch (error) {
+      res.status(404).json({ error });
+    };
 })
 
 

e10f04546c76086580c7a8d3fe70856f929f0768
sasd <jayceon310893@gmail.com>
2020-12-22 21:48:35 +0300

    v1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,48 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 console.log('webpack starterkit');
+
+///структура данных
+
+let questions = [
+  {
+    category: 'Охота и рыбалка',
+    question: 'Карась или щука?',
+    imgage: 'some image',
+		responses: [
+			{id: 'asd', text: 'str2', correct: false},
+			{id: 'sdf', text: 'str1', correct: true},
+			{id: 'wer', text: 'str3', correct: false},
+		]
+  },
+  {
+    category: 'Фотография',
+    question: 'Плёнка или цифра',
+    imgage: 'some image',
+		responses: [
+			{id: 'asd', text: 'sertr2', correct: false},
+			{id: 'sdf', text: 'stref1', correct: true},
+			{id: 'wer', text: 'sewftr3', correct: false},
+		]
+  },
+];
+
+
+function getAllquestions() {
+  return qustions.map(question => getQuestionHtml(question)).join('');
+}
+
+function getQuestionHtml(question) {
+  debugger
+  return `<div class="qustion__container">
+    <div class="question__title">${question.question}</div>     
+  </div>`;
+}
+
+const app = document.querySelector('.app');
+
+function render() {
+  app.innerHTML = getQuestionHtml(questions[0]);
+}
+
+render();
